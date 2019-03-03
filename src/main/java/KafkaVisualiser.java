11b63917a18c2566393eae1282ff21b43fb0412d
adam dal <finallytrycatch@outlook.com>
2019-03-03 10:39:22 +0000

    Initial commit

---
+++ src/main/java/KafkaVisualiser.java
@@ -0,0 +1,7 @@
+public class KafkaVisualiser {
+
+    public static void main(String[] args) {
+        System.out.println("Hello World");
+
+    }
+}
---
+++ src/test/java/KafkaVisualiserTest.java
@@ -0,0 +1,10 @@
+import static org.junit.Assert.assertEquals;
+import org.junit.Test;
+
+public class KafkaVisualiserTest {
+
+    @Test
+    public void evaluatesExpression() {
+        assertEquals(6, 3+3);
+    }
+}
